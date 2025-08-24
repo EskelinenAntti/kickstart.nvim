00136463499d33e89b200b43298d40d9fc0159c7
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-08-24 09:40:58 +0300

    ktfmt

--- init.lua
+++ init.lua
@@ -770,7 +770,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        kotlin = { 'ktlint' },
+        kotlin = { 'ktfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
